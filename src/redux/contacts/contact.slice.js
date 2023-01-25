5a86b89b4485cd8803fb5e0794cf78f5aef478e2
Dima Telechkun <telechkun93@gmail.com>
2023-01-25 21:05:34 +0200

    contact slice update

--- src/redux/contacts/contact.slice.js
+++ src/redux/contacts/contact.slice.js
@@ -1,8 +1,11 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { getContactsThunk } from 'redux/thunks/contactsThunk';
 
 const contactSlice = createSlice({
   name: 'contacts',
   initialState: {
+    error: null,
+    isLoading: false,
     items: [],
     filter: '',
   },
@@ -24,6 +27,22 @@ const contactSlice = createSlice({
         contact => contact.id !== action.payload
       );
     },
+  },
+  extraReducers: builder => {
+    builder
+      .addCase(getContactsThunk.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(getContactsThunk.fulfilled, (state, action) => {
+        state.contacts = action.payload;
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(getContactsThunk.rejected, (state, action) => {
+        state.contacts = [];
+        state.error = action.payload;
+        state.isLoading = false;
+      });
   },
 });
 
